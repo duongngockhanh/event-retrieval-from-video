cd5949286036c0b6032d5e0067f2edf65d7ee779
duongngockhanh <khanhduongsc@gmail.com>
2023-08-06 23:03:58 +0700

    update

--- demo.py
+++ demo.py
@@ -1,14 +1,19 @@
-import json
+# import json
 
-with open('image_path.json') as json_file:
-    DictImagePath = json.load(json_file)
+# with open('image_path.json') as json_file:
+#     DictImagePath = json.load(json_file)
 
-# print(type(DictImagePath))
-id2img_fps = {}
-for key, value in DictImagePath.items():
-   id2img_fps[int(key)] = value 
+# # print(type(DictImagePath))
+# id2img_fps = {}
+# for key, value in DictImagePath.items():
+#    id2img_fps[int(key)] = value 
 
-print(id2img_fps)
-with open('image_path_1.json', 'a+') as f:
-  f.write(json.dumps(id2img_fps))
+# print(id2img_fps)
+# with open('image_path_1.json', 'a+') as f:
+#   f.write(json.dumps(id2img_fps))
 
+import glob
+
+lis = glob.glob("images/*.jpg")
+
+print(len(lis))
