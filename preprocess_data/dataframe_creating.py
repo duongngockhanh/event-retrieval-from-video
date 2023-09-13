a20110e17ae170e0b12932c726c1f5a1fadcfcfe
duongngockhanh <khanhduongsc@gmail.com>
2023-09-13 16:16:52 +0700

    update dataframe_creating

--- preprocess_data/dataframe_creating.py
+++ preprocess_data/dataframe_creating.py
@@ -13,28 +13,33 @@ cols_name = class_list + ["image_path"]
 schema = [(col_name, pl.UInt8) if col_name!="image_path" else (col_name, pl.Utf8) for col_name in cols_name]
 df = pl.DataFrame({}, schema=schema)
 
-first_name = "detection_L03"
-second_name_list = sorted(os.listdir(first_name))
+csv_path = "dataframe_Lxx.csv"
+first_path = "detection_Lxx"
+first_path_image = "compressed_Lxx"
+second_name_list = sorted(os.listdir(first_path))
+second_name_list = [item for item in second_name_list if not item.startswith('.')]
+for second_name in second_name_list:
+    second_path = os.path.join(first_path, second_name)
+    third_name_list = sorted(os.listdir(second_path))
+    third_name_list = [item for item in third_name_list if not item.startswith('.')]
 
-first_name_image = "L03_keyframes"
-
-for second_name in tqdm(second_name_list):
-    third_name_list = sorted(os.listdir(os.path.join(first_name, second_name)))
-    
-    for third_name in third_name_list:
-        temp_dict = {key: 0 for key in cols_name}
-        temp_dict["image_path"] = os.path.join(first_name_image, second_name, third_name[:-4] + ".jpg")
-        third_path = os.path.join(first_name, second_name, third_name)
+    for third_name in tqdm(third_name_list):
+        third_path = os.path.join(second_path, third_name)
+        forth_name_list = sorted(os.listdir(third_path))
         
-        with open(third_path) as f:
-            for line in f:
-                idx_class = int(line.split()[0])
-                temp_dict[cols_name[idx_class]] = temp_dict.get(cols_name[idx_class], 0) + 1
+        for forth_name in forth_name_list:
+            temp_dict = {key: 0 for key in cols_name}
+            temp_dict["image_path"] = os.path.join(first_path_image, second_name, third_name, forth_name[:-4] + ".jpg")
+            forth_path = os.path.join(third_path, forth_name)
+            
+            with open(forth_path) as f:
+                for line in f:
+                    idx_class = int(line.split()[0])
+                    temp_dict[cols_name[idx_class]] = temp_dict.get(cols_name[idx_class], 0) + 1
 
-        temp_df = pl.DataFrame([temp_dict], schema=schema)
-        df = df.extend(temp_df)
+            temp_df = pl.DataFrame([temp_dict], schema=schema)
+            df = df.extend(temp_df)
 
 
-csv_path = "dataframe_L03.csv"
 df.write_csv(csv_path, separator=",")
 print(df.estimated_size())
--- utils/detection.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import polars as pl
-from functools import reduce
-
-# class Detection:
-#     def __init__(self) -> None:
-#         self.
