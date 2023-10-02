c3860d36c05f1fb3567e324b2bb95e95edaadf97
duongngockhanh <khanhduongsc@gmail.com>
2023-10-02 12:10:44 +0700

    update create_submission.py

--- custom_submission/create_submission.py
+++ custom_submission/create_submission.py
@@ -6,4 +6,4 @@ os.makedirs(submission_folder)
 n = 30
 
 for i in range(1, n+1):
-    os.system(f"touch {submission_folder}/query-p2-{i}.csv")
+    os.system(f"touch {submission_folder}/query-p3-{i}.csv")
